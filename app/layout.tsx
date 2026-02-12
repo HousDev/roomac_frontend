e423ea1930b926360490fc2d14748d000daad074
Heena <heena@hously.in>
2026-02-12 10:24:53 +0530

    layout changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,23 +19,14 @@ export default function RootLayout() {
   const pathname = usePathname();
   const isAdminRoute = pathname?.startsWith("/admin");
   const isTenantRoute = pathname?.includes("/tenant");
-  const [checking, setChecking] = useState(true);
-  useEffect(() => {
-    const token = localStorage.getItem("admin_token");
 
-    if (!token) {
-      window.location.href = "/admin/login";
-    }else {
-      setChecking(false);
-    }
-  }, []);
   return (
     <div>
-    {checking?<div>loading</div>:<div className={poppins.className}>
+    <div className={poppins.className}>
       {!isAdminRoute && !isTenantRoute && <Header />}
       <main className="min-h-screen"><Outlet /></main>
       {!isAdminRoute && !isTenantRoute && <Footer />}
-    </div>}
+    </div>
     </div>
   );
 }
