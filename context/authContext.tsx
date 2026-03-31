23743eff9786d154da427bc99b0eb633555ce5a3
Kamlesh7049 <kamleshprasadshah56@gmail.com>
2026-03-31 16:33:55 +0530

    error catch

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -37,10 +37,11 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   
   const fetchUser = async () => {try{
 const user = await axios.get(import.meta.env.VITE_API_URL+"/api/auth/get-user-details/"+localStorage.getItem('auth_email'))
-console.log(user.data.user)
-   const userData = user.data.user;
-   userData.name = userData.name.split(" ").map((n:string) => n.charAt(0).toUpperCase()+n.slice(1)).join(" ")
-    setUser(userData)
+    // console.log(user.data.user)
+    //    const userData = user.data.user;
+    //    userData.name = userData.name.split(" ").map((n:string) => n.charAt(0).toUpperCase()+n.slice(1)).join(" ")
+
+    setUser(user.data.user)
 }catch(error){
   console.log(error)
 }
